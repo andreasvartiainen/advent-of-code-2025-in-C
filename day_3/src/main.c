72df83c1b87d28b818f935f808c84cc3f141e379
Tomi <59865056+andreasvartiainen@users.noreply.github.com>
2025-12-05 11:34:20 +0200

    day_3 part 2 completed

--- day_3/src/main.c
+++ day_3/src/main.c
@@ -19,34 +19,55 @@ int value_from_string(const char *str) {
 	return value;
 }
 
-int calculate_joltage(const char *line) {
+int64_t calculate_joltage(const char *line, int battery_count) {
 	int len = (int)strlen(line);
-	// printf("LINE LENGTH: %d\n", len);
+	// { 3 (>3) (>4) (>5) ... (>n) }
+	// battery_index saves the indexes of the batteries
+	int *battery_index = (int*)malloc(sizeof(int) * battery_count);
+	for (int i = 0; i < battery_count; i++) {
+		battery_index[i] = 0;
+	}
 	
-	int max_index = 0;
-	for (int i = 0; i < len - 1; i++) {
-		if (i_from_c(line[i]) > i_from_c(line[max_index])) {
-			max_index = i;
+	// this goes through all the batteries in order
+	for (int bat = 0; bat < battery_count; bat++) {
+		// set the current battery index to be one more than the last one
+		// to ignore the previous index
+		if (bat != 0) {
+			battery_index[bat] = battery_index[bat - 1] + 1;
+		}
+		// this i starts looking at the line[i] from the index in current
+		// battery index (bat)
+		// when looking at the first battery we have to return the first index
+		for (int i = (bat == 0) 
+				? battery_index[bat] 
+				: battery_index[bat-1] + 1; // this should look behind one and 
+																		// add one to the index
+				i < len - battery_count + bat + 1; // len - battery_count
+				i++
+				) {
+			// if the current item line[i] is bigger than line[battery_index[bat]
+			// save the index to the battery_index[bat]
+			// here the second round starts from 0 index again
+			// because battery_index[bat] = 0 so we compare the first item again
+			if (line[i] > line[battery_index[bat]]) {
+				battery_index[bat] = i;
+			}
 		}
 	}
 
-	// we start this search from where last stopped
-	int max_index2 = max_index + 1;
-	for (int i = max_index2; i < len; i++) {
-		if (i_from_c(line[i]) > i_from_c(line[max_index2])) {
-			max_index2 = i;
-		}
+	int64_t return_val = 0;
+	const int ten_mul = 10;
+	for (int i = 0; i < battery_count; i++) {
+		// 0 + 9 = 9
+		// 9 * 10 = 90
+		// 90 + 5 = 95
+		// ...
+		return_val += i_from_c(line[battery_index[i]]);
+		return_val *= ten_mul;
 	}
+	return_val /= ten_mul;
 
-	// printf("%s\n", line);
-	// printf("max 1: %d max 2: %d\n", i_from_c(line[max_index]), i_from_c(line[max_index2]));
-
-	int return_val = 0;
-	return_val = i_from_c(line[max_index]);
-	const int ten_div = 10;
-	return_val *= ten_div;
-	return_val += i_from_c(line[max_index2]);
-
+	free(battery_index);
 	return return_val;
 }
 
@@ -62,17 +83,18 @@ int main() {
 		return fprintf(stderr, "ERROR: when opening the file %s for reading\n", FILENAME); 
 	}
 
-	int sum = 0;
+	int64_t sum = 0;
+	const int battery_count = 12;
 	while (!feof(file)) {
 		char buf[BUF_SIZE] = {};
 		if(!fgets(buf, BUF_SIZE, file)) {
 			break;
 		}
 		strip_newline(buf);
-		sum += calculate_joltage(buf);
+		sum += calculate_joltage(buf, battery_count);
 	}
 
-	printf("joltake rating: %d", sum);
+	printf("joltake rating: %ld", sum);
 
 	if (fclose(file) == EOF) {
 		return fprintf(stderr, "ERROR: when closing the file %s\n", FILENAME); 
